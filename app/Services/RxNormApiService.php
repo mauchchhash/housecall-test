9e4a62ab67dfa906500872d3379a9f85618b1736
Mahim Anjum Uchchhash <51711462+mauchchhash@users.noreply.github.com>
2023-12-13 18:49:24 +0600

    Implemented caching for public drug search api

--- app/Services/RxNormApiService.php
+++ app/Services/RxNormApiService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
+use PhpParser\Node\Stmt\TryCatch;
 
 class RxNormApiService
 {
@@ -14,23 +15,32 @@ class RxNormApiService
      */
     public function searchDrugs(string $drugName): array
     {
-        $responseData = Http::RxNormApi()->get('/drugs.json', [
-            'name' => $drugName
-        ])->json();
-        $conceptGroup = $responseData['drugGroup']['conceptGroup'];
-        $sbdData = collect($conceptGroup)->filter(function ($cg) {
-            return $cg['tty'] === 'SBD';
-        })->first();
-        $topFiveDrugs = array_slice($sbdData['conceptProperties'], 0, 5);
-        foreach ($topFiveDrugs as &$d) {
-            unset($d['synonym'], $d['tty'], $d['language'], $d['suppress'], $d['umlscui']);
+        try {
+            if (cache($drugName)) {
+                $topFiveDrugs = cache($drugName);
+            } else {
+                $responseData = Http::RxNormApi()->get('/drugs.json', [
+                    'name' => $drugName
+                ])->json();
+                $conceptGroup = $responseData['drugGroup']['conceptGroup'];
+                $sbdData = collect($conceptGroup)->filter(function ($cg) {
+                    return $cg['tty'] === 'SBD';
+                })->first();
+                $topFiveDrugs = array_slice($sbdData['conceptProperties'], 0, 5);
+                foreach ($topFiveDrugs as &$d) {
+                    unset($d['synonym'], $d['tty'], $d['language'], $d['suppress'], $d['umlscui']);
 
-            $history = $this->getRxcuiHistoryStatus($d['rxcui']);
+                    $history = $this->getRxcuiHistoryStatus($d['rxcui']);
 
-            $d['baseNames'] = $this->getBaseNamesFromHistory($history);
-            $d['doseFormGroupNames'] = $this->getDoseFormGroupNamesFromHistory($history);
+                    $d['baseNames'] = $this->getBaseNamesFromHistory($history);
+                    $d['doseFormGroupNames'] = $this->getDoseFormGroupNamesFromHistory($history);
+                }
+                cache([$drugName => $topFiveDrugs], now()->addMinutes(60));
+            }
+            return $topFiveDrugs;
+        } catch (\Throwable $th) {
+            throw $th;
         }
-        return $topFiveDrugs;
     }
 
     /**
@@ -41,8 +51,12 @@ class RxNormApiService
      */
     public function getRxcuiHistoryStatus($rxcui)
     {
-        $responseData = Http::RxNormApi()->get("/rxcui/{$rxcui}/historystatus.json")->json();
-        return $responseData;
+        try {
+            $responseData = Http::RxNormApi()->get("/rxcui/{$rxcui}/historystatus.json")->json();
+            return $responseData;
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
@@ -53,8 +67,12 @@ class RxNormApiService
      */
     public function getBaseNamesFromHistory($history)
     {
-        $ingredientAndStrength = $history['rxcuiStatusHistory']['definitionalFeatures']['ingredientAndStrength'];
-        return $ingredientAndStrength;
+        try {
+            $ingredientAndStrength = $history['rxcuiStatusHistory']['definitionalFeatures']['ingredientAndStrength'];
+            return $ingredientAndStrength;
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
@@ -65,9 +83,13 @@ class RxNormApiService
      */
     public function getDoseFormGroupNamesFromHistory($history)
     {
-        $doseFormGroupConcept = $history['rxcuiStatusHistory']['definitionalFeatures']['doseFormGroupConcept'];
-        return array_map(function ($i) {
-            return $i['doseFormGroupName'];
-        }, $doseFormGroupConcept);
+        try {
+            $doseFormGroupConcept = $history['rxcuiStatusHistory']['definitionalFeatures']['doseFormGroupConcept'];
+            return array_map(function ($i) {
+                return $i['doseFormGroupName'];
+            }, $doseFormGroupConcept);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 }
